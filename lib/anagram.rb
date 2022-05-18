f51023d7026c7907d8f9904dd65c3ba8ad85a8b7
imj22 <98693440+imj22@users.noreply.github.com>
2022-05-18 12:43:28 +0200

    commit tree 3c2fb47213cc343c46aa382d6d74847df4d309b8

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
 # Your code goes here!
+class Anagram 
+
+    attr_accessor :word
+
+
+    def initialize (word)
+        @word = word
+    end
+
+    def matches (arr_of_words)
+        arr_of_words.collect do |word|
+            word.chars.sort == @word.chars.sort
+        end
+    end
+end
+
+diaper = Anagram.new("diaper")
+puts diaper.matches(%w[hello world zombies pants dipper])
+
